368e4d95b6bcf01f7e99900e33d109155931954d
lambdamusic <michele.pasin@gmail.com>
2018-06-18 14:33:54 +0100

    parsing message

--- ontospy/main.py
+++ ontospy/main.py
@@ -39,9 +39,6 @@ from .core.ontospy import Ontospy
 from .core.manager import *
 from .core.utils import *
 
-
-
-
 SHELL_EXAMPLES = """
 Quick Examples:
 
@@ -53,34 +50,83 @@ More info: <ontospy.readthedocs.org>
 ------------
 """
 
-
-
-
 ##################
 #
 #  COMMAND LINE MAIN METHODS
 #
 ##################
 
-
-
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument('sources', nargs=-1)
-@click.option('--bootstrap', '-b', is_flag=True, help='BOOTSTRAP: bootstrap the local library.')
-@click.option('--cache', '-c', is_flag=True, help='CACHE: force caching of the local library (for faster loading).')
-@click.option('--delete', '-d', is_flag=True, help='DELETE: remove an ontology from the local library.')
-@click.option('--endpoint', '-e', is_flag=True, help='ENDPOINT: query a sparql endpoint.')
-@click.option('--library', '-l', is_flag=True, help='LIBRARY: list ontologies saved in the local library.')
-@click.option('--save', '-s', is_flag=True, help='SAVE: save a file/folder/url into the local library.')
-@click.option('--reset', '-r', is_flag=True, help='RESET: delete all files in the local library.')
-@click.option('--update', '-u', is_flag=True, help='UPDATE: enter new path for the local library.')
-@click.option('--verbose', '-v', is_flag=True, help='VERBOSE: verbose logs and entities labels.')
-@click.option('--web', '-w', is_flag=True, help='WEB: import ontologies from remote repositories.')
-@click.option('--shell', '-z', is_flag=True, help='SHELL: launch the interactive shell.')
+@click.option(
+    '--bootstrap',
+    '-b',
+    is_flag=True,
+    help='BOOTSTRAP: bootstrap the local library.')
+@click.option(
+    '--cache',
+    '-c',
+    is_flag=True,
+    help='CACHE: force caching of the local library (for faster loading).')
+@click.option(
+    '--delete',
+    '-d',
+    is_flag=True,
+    help='DELETE: remove an ontology from the local library.')
+@click.option(
+    '--endpoint',
+    '-e',
+    is_flag=True,
+    help='ENDPOINT: query a sparql endpoint.')
+@click.option(
+    '--library',
+    '-l',
+    is_flag=True,
+    help='LIBRARY: list ontologies saved in the local library.')
+@click.option(
+    '--save',
+    '-s',
+    is_flag=True,
+    help='SAVE: save a file/folder/url into the local library.')
+@click.option(
+    '--reset',
+    '-r',
+    is_flag=True,
+    help='RESET: delete all files in the local library.')
+@click.option(
+    '--update',
+    '-u',
+    is_flag=True,
+    help='UPDATE: enter new path for the local library.')
+@click.option(
+    '--verbose',
+    '-v',
+    is_flag=True,
+    help='VERBOSE: verbose logs and entities labels.')
+@click.option(
+    '--web',
+    '-w',
+    is_flag=True,
+    help='WEB: import ontologies from remote repositories.')
+@click.option(
+    '--shell', '-z', is_flag=True, help='SHELL: launch the interactive shell.')
 @click.pass_context
-def main_cli(ctx, sources=None, library=False, verbose=False, save=False, bootstrap=False, web=False, shell=False, cache=False, update=False, delete=False, reset=False, endpoint=False):
+def main_cli(ctx,
+             sources=None,
+             library=False,
+             verbose=False,
+             save=False,
+             bootstrap=False,
+             web=False,
+             shell=False,
+             cache=False,
+             update=False,
+             delete=False,
+             reset=False,
+             endpoint=False):
     """
 Ontospy is a command line inspector for RDF/OWL knowledge models.
 
@@ -107,16 +153,17 @@ More info: <ontospy.readthedocs.org>
     if not update:
         get_or_create_home_repo()
     print_opts = {
-                    'labels' : verbose,
-                }
+        'labels': verbose,
+    }
 
-    click.secho("OntoSpy " + VERSION,  bold=True)
+    click.secho("OntoSpy " + VERSION, bold=True)
     # click.secho("Local library: '%s'" % get_home_location(), fg='white')
     click.secho("------------", fg='white')
 
     if bootstrap:
         action_bootstrap()
-        printDebug("Tip: you can now load an ontology by typing `ontospy -l`", "important")
+        printDebug("Tip: you can now load an ontology by typing `ontospy -l`",
+                   "important")
         # raise SystemExit(1)
 
     elif cache:
@@ -143,7 +190,8 @@ More info: <ontospy.readthedocs.org>
 
     elif update:
         if not sources:
-            printDebug("Please specify a new location for the local library.", 'important')
+            printDebug("Please specify a new location for the local library.",
+                       'important')
             printDebug("E.g. 'ontospy -u /Users/john/ontologies'", 'tip')
             sys.exit(0)
         else:
@@ -153,14 +201,17 @@ More info: <ontospy.readthedocs.org>
                 _location = _location[:-1]
             output = action_update_library_location(_location)
             if output:
-                printDebug("Note: no files have been moved or deleted (this has to be done manually)", "comment")
-                printDebug("----------\n" + "New location: '%s'" % _location, "important")
+                printDebug(
+                    "Note: no files have been moved or deleted (this has to be done manually)",
+                    "comment")
+                printDebug("----------\n" + "New location: '%s'" % _location,
+                           "important")
 
             else:
-                printDebug("----------\n" + "Please specify an existing folder path.", "important")
+                printDebug(
+                    "----------\n" + "Please specify an existing folder path.",
+                    "important")
             raise SystemExit(1)
-
-
 
     elif web:
         action_webimport()
@@ -172,8 +223,9 @@ More info: <ontospy.readthedocs.org>
 
     else:
         if sources or (sources and endpoint):
-            t = "You passed the arguments:%s" % "".join([ "\n-> <%s>" % str(x) for x in sources])
-            click.secho(str(t), fg='green')
+            for x in sources:
+                click.secho("Parsing %s..." % str(x), fg='white')
+
             if endpoint:
                 g = Ontospy(sparql_endpoint=sources[0], verbose=verbose)
                 printDebug("Extracting classes info")
@@ -191,13 +243,10 @@ More info: <ontospy.readthedocs.org>
             click.echo(ctx.get_help())
             return
 
-
     # finally: print(some stats.... )
     eTime = time.time()
     tTime = eTime - sTime
-    printDebug("\n----------\n" + "Time:	   %0.2fs" %  tTime, "comment")
-
-
+    printDebug("\n----------\n" + "Time:	   %0.2fs" % tTime, "comment")
 
 
 if __name__ == '__main__':
@@ -205,5 +254,5 @@ if __name__ == '__main__':
     try:
         main_cli(prog_name='ontospy')
         sys.exit(0)
-    except KeyboardInterrupt as e: # Ctrl-C
+    except KeyboardInterrupt as e:  # Ctrl-C
         raise e
