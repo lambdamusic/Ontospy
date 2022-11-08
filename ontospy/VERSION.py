f65f34813de2936be30b2ddae7087f6d546e7f0f
lambdamusic <michele.pasin@gmail.com>
2022-11-08 18:57:46 +0000

    v2.0.1

--- ontospy/VERSION.py
+++ ontospy/VERSION.py
@@ -8,7 +8,7 @@
 #  note: 1.8 major update with ontodocs split
 ##################
 
-__version__ = "2.0.0"  # Pypi latest: https://pypi.org/project/ontospy/
+__version__ = "2.0.1"  # Pypi latest: https://pypi.org/project/ontospy/
 
 __copyright__ = "CopyRight (C) 2015-2022 by Michele Pasin"
 __license__ = "GNU"
